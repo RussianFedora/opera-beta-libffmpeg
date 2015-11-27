1e59d6f7d1b308ce3097b40d1b173255cc8d6df0
berlogue <carasin.berlogue@mail.ru>
2015-11-27 19:38:33 +0300

    Update to 34.0.2036.16

--- check_chromium_version.sh
+++ check_chromium_version.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
+BUILD_FROM_RPM=0
 mkdir -p ./opera-beta-$1
 pushd ./opera-beta-$1 &> /dev/null
 echo -en "\033[0;35m    Downloading Opera Beta package:\033[0m\n"
-wget -N -q --show-progress ftp://ftp.opera.com/pub/opera-beta/$1/linux/opera-beta_$1_amd64.deb
-echo -en "\033[0;35m    Opera Beta package hash:\033[0m\n"
-echo -en "\033[0;32m$(md5sum opera-beta_$1_amd64.deb)\033[0m\n"
-ar p opera-beta_$1_amd64.deb data.tar.xz | tar -xJf-
-CHROMIUM_VER=$(strings ./usr/lib/x86_64-linux-gnu/opera-beta/opera-beta | grep Chrome/ | cut --delimiter=/ --fields=2)
+if [ "$BUILD_FROM_RPM" = 1 ]; then
+    wget -N -q --show-progress ftp://ftp.opera.com/pub/opera-beta/$1/linux/opera-beta_$1_amd64.rpm
+    echo -en "\033[0;35m    Opera Beta RPM package hash:\033[0m\n"
+    echo -en "\033[0;32m$(md5sum opera-beta_$1_amd64.rpm)\033[0m\n"
+    rpm2cpio opera-beta_$1_amd64.rpm | cpio -idV --quiet
+    CHROMIUM_VER=$(strings ./usr/lib64/opera-beta/opera-beta | grep Chrome/ | cut --delimiter=/ --fields=2)
+else
+    wget -N -q --show-progress ftp://ftp.opera.com/pub/opera-beta/$1/linux/opera-beta_$1_amd64.deb
+    echo -en "\033[0;35m    Opera Beta DEB package hash:\033[0m\n"
+    echo -en "\033[0;32m$(md5sum opera-beta_$1_amd64.deb)\033[0m\n"
+    ar p opera-beta_$1_amd64.deb data.tar.xz | tar -xJf-
+    CHROMIUM_VER=$(strings ./usr/lib/x86_64-linux-gnu/opera-beta/opera-beta | grep Chrome/ | cut --delimiter=/ --fields=2)
+fi
 echo -en "\033[0;35m    Chromium version:\033[0m\n"
 echo -en "\033[0;32m$(echo $CHROMIUM_VER)\033[0m\n"
 echo -en "\033[0;35m    Downloading Chromium sources archive:\033[0m\n"
